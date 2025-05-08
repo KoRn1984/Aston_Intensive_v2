a1960a503cf063259843b2dadc40b6310a5f2b3d
KoRn <korn1984@narod.ru>
2025-05-08 12:31:08 +0300

    Minor final adjustments.

--- task3/src/main/java/ru/aston/matveenko_ym/repository/UserRepository.java
+++ task3/src/main/java/ru/aston/matveenko_ym/repository/UserRepository.java
@@ -1,7 +1,9 @@
 package ru.aston.matveenko_ym.repository;
 
+import org.springframework.stereotype.Repository;
 import ru.aston.matveenko_ym.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
