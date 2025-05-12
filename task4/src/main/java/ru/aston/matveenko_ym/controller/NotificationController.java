d8bf83b0af190d046999232dcf7ba1ffe43a42a3
KoRn <korn1984@narod.ru>
2025-05-12 18:35:34 +0300

    NotificationService (edits).

--- task4/src/main/java/ru/aston/matveenko_ym/controller/NotificationController.java
+++ task4/src/main/java/ru/aston/matveenko_ym/controller/NotificationController.java
@@ -1,5 +1,7 @@
 package ru.aston.matveenko_ym.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +12,7 @@ import ru.aston.matveenko_ym.service.EmailService;
 @RequestMapping("/api/v1/notifications")
 public class NotificationController {
 
+    private static final Logger log = LoggerFactory.getLogger(NotificationController.class);
     private final EmailService emailService;
 
     public NotificationController(EmailService emailService) {
@@ -18,7 +21,11 @@ public class NotificationController {
 
     @PostMapping("/send")
     public String sendEmail(@RequestParam String to, @RequestParam String subject, @RequestParam String text) {
-        emailService.sendEmail(to, subject, text);
+        try {
+            emailService.sendEmail(to, subject, text);
+        } catch (Exception exception) {
+            log.error("Error sending email: {}", exception.getMessage());
+        }
         return "Email sent successfully!";
     }
 }
